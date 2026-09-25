75295e14e455785549454d45a791de2ec84b5cf3
Mikolaj Chudzinski <m.chudzinski26@gmail.com>
2026-09-25 13:23:45 +0200

    Javascript in oop gezet

--- script.js
+++ script.js
@@ -1,231 +1,122 @@
-let cookie = 0;
-let cookiePerClick = 1;
+class Upgrade {
+    constructor(game, { id, countId, name, cost, costIncrease }) {
+        this.game = game;
+        this.button = document.getElementById(id);
+        this.count = document.getElementById(countId);
+        this.name = name;
+        this.cost = cost;
+        this.costIncrease = costIncrease;
+        this.owned = 0;
 
-const cookieButton = document.getElementById('cookiebutton');
-const cookieCount = document.getElementById('cookiecount');
+        this.button.addEventListener('click', () => this.buy());
+        this.render();
+    }
 
-function showUpgradeEffect(button) {
-    button.classList.remove('upgrade-purchased');
-    void button.offsetWidth;
-    button.classList.add('upgrade-purchased');
+    buy() {
+        if (this.game.stardust < this.cost) {
+            return;
+        }
+
+        this.game.stardust -= this.cost;
+        this.owned++;
+        this.cost += this.costIncrease;
+        this.render();
+        this.game.renderStardust();
+        this.showPurchaseEffect();
+        this.onPurchase();
+    }
+
+    onPurchase() {
+        
+    }
+
+    render() {
+        this.button.textContent = `${this.name}: ${this.cost} stardust`;
+        this.count.textContent = this.owned;
+    }
+
+    showPurchaseEffect() {
+        this.button.classList.remove('upgrade-purchased');
+        void this.button.offsetWidth;
+        this.button.classList.add('upgrade-purchased');
+    }
 }
 
-cookieButton.addEventListener('click', () => {
-    cookie += cookiePerClick;
-    cookieCount.textContent = cookie;
-});
-
-const upgradeButton = document.getElementById('upgrade');
-const upgradeCount = document.getElementById('upgradecount');
-let upgradeCost = 50;
-let upgradesOwned = 0;
-
-upgradeButton.textContent = `Upgrade klik: ${upgradeCost} stardust`;
-
-upgradeButton.addEventListener('click', () => {
-    if (cookie >= upgradeCost) {
-        cookie -= upgradeCost;
-        cookiePerClick++;
-        upgradesOwned++;
-        upgradeCost += 50;
-        upgradeCount.textContent = upgradesOwned;
-        cookieCount.textContent = cookie;
-        upgradeButton.textContent = `Upgrade klik: ${upgradeCost} stardust`;
-        showUpgradeEffect(upgradeButton);
+class ClickUpgrade extends Upgrade {
+    constructor(game, settings) {
+        super(game, settings);
+        this.button.textContent = `Upgrade klik: ${this.cost} stardust`;
     }
-});
 
-const grandmaButton = document.getElementById('grandma');
-const grandmaCount = document.getElementById('grandmacount');
-let grandmaCost = 200;
-let grandmaOwned = 0;
-
-grandmaButton.textContent = `Asteroids: ${grandmaCost} stardust`;
-
-grandmaButton.addEventListener('click', () => {
-    if (cookie >= grandmaCost) {
-        cookie -= grandmaCost;
-        grandmaOwned++;
-        grandmaCount.textContent = grandmaOwned;
-        grandmaCost += 50;
-        grandmaButton.textContent = `Asteroids: ${grandmaCost} stardust`;
-        showUpgradeEffect(grandmaButton);
+    onPurchase() {
+        this.game.cookiePerClick++;
     }
-});
+}
 
-let grandmaInterval = setInterval(() => {
-    cookie += 3 * grandmaOwned;
-    cookieCount.textContent = cookie;
-}, 1000);
-
-const starButton = document.getElementById('star');
-const starCount = document.getElementById('starcOUNT');
-let starCost = 600;
-let starOwned = 0;
-
-starButton.addEventListener('click', () => {
-    if (cookie >= starCost) {
-        cookie -= starCost;
-        starOwned++;
-        starCount.textContent = starOwned;
-        starCost += 300;
-        starButton.textContent = `Stars: ${starCost} stardust`;
-        showUpgradeEffect(starButton);
+class ProductionUpgrade extends Upgrade {
+    constructor(game, settings) {
+        super(game, settings);
+        this.production = settings.production;
     }
-});
 
-let starInterval = setInterval(() => {
-    cookie += 60 * starOwned;
-    cookieCount.textContent = cookie;
-}, 1000);
-
-const factoryButton = document.getElementById('factory');
-const factoryCount = document.getElementById('factorycount');
-let factoryCost = 1000;
-let factoryOwned = 0;
-
-factoryButton.addEventListener('click', () => {
-    if (cookie >= factoryCost) {
-        cookie -= factoryCost;
-        factoryOwned++;
-        factoryCount.textContent = factoryOwned;
-        factoryCost += 500;
-        factoryButton.textContent = `Nebulas: ${factoryCost} stardust`;
-        showUpgradeEffect(factoryButton);
+    produce() {
+        return this.production * this.owned;
     }
-});
+}
 
-let factoryInterval = setInterval(() => {
-    cookie += 100 * factoryOwned;
-    cookieCount.textContent = cookie;
-}, 1000);
+class Game {
+    constructor() {
+        this.stardust = 0;
+        this.cookiePerClick = 1;
+        this.cookieButton = document.getElementById('cookiebutton');
+        this.cookieCount = document.getElementById('cookiecount');
+        this.upgrades = [];
 
-const GalaxiesButton = document.getElementById('Galaxies');
-const GalaxiesCount = document.getElementById('Galaxiescount');
-let GalaxiesCost = 6000;
-let GalaxiesOwned = 0;
-
-GalaxiesButton.addEventListener('click', () => {
-    if (cookie >= GalaxiesCost) {
-        cookie -= GalaxiesCost;
-        GalaxiesOwned++;
-        GalaxiesCount.textContent = GalaxiesOwned;
-        GalaxiesCost += 1200;
-        GalaxiesButton.textContent = `Galaxies: ${GalaxiesCost} stardust`;
-        showUpgradeEffect(GalaxiesButton);
+        this.cookieButton.addEventListener('click', () => this.clickForStardust());
+        this.createUpgrades();
+        this.renderStardust();
+        setInterval(() => this.collectProduction(), 1000);
     }
-});
+    createUpgrades() {
+        this.upgrades.push(new ClickUpgrade(this, {
+            id: 'upgrade', countId: 'upgradecount', name: 'Upgrade klik', cost: 50, costIncrease: 50
+        }));
 
-let GalaxiesInterval = setInterval(() => {
-    cookie += 400 * GalaxiesOwned;
-    cookieCount.textContent = cookie;
-}, 1000);
+        const productionSettings = [
+            { id: 'grandma', countId: 'grandmacount', name: 'Asteroids', cost: 200, costIncrease: 50, production: 3 },
+            { id: 'star', countId: 'starcOUNT', name: 'Stars', cost: 600, costIncrease: 300, production: 60 },
+            { id: 'factory', countId: 'factorycount', name: 'Nebulas', cost: 1000, costIncrease: 500, production: 100 },
+            { id: 'Galaxies', countId: 'Galaxiescount', name: 'Galaxies', cost: 6000, costIncrease: 1200, production: 400 },
+            { id: 'Black holes', countId: 'Black holescount', name: 'Black Holes', cost: 15000, costIncrease: 2600, production: 1000 },
+            { id: 'Galaxy Clusters', countId: 'Galaxy Clusterscount', name: 'Galaxy Clusters', cost: 24000, costIncrease: 3000, production: 2000 },
+            { id: 'Superclusters', countId: 'Superclusterscount', name: 'Superclusters', cost: 50000, costIncrease: 5000, production: 3000 },
+            { id: 'The Observable Universe', countId: 'The Observable Universecount', name: 'The Observable Universe', cost: 100000, costIncrease: 10000, production: 5000 },
+            { id: 'Multiverse', countId: 'Multiversecount', name: 'Multiverse', cost: 250000, costIncrease: 25000, production: 15000 }
+        ];
 
-const BlackHolesButton = document.getElementById('Black holes');
-const BlackHolesCount = document.getElementById('Black holescount');
-let BlackHolesCost = 15000;
-let BlackHolesOwned = 0;
-
-BlackHolesButton.addEventListener('click', () => {
-    if (cookie >= BlackHolesCost) {
-        cookie -= BlackHolesCost;
-        BlackHolesOwned++;
-        BlackHolesCount.textContent = BlackHolesOwned;
-        BlackHolesCost += 2600;
-        BlackHolesButton.textContent = `Black Holes: ${BlackHolesCost} stardust`;
-        showUpgradeEffect(BlackHolesButton);
+        productionSettings.forEach(settings => {
+            this.upgrades.push(new ProductionUpgrade(this, settings));
+        });
     }
-});
 
-let BlackHolesInterval = setInterval(() => {
-    cookie += 1000 * BlackHolesOwned;
-    cookieCount.textContent = cookie;
-}, 1000);
-
-const GalaxyClustersButton = document.getElementById('Galaxy Clusters');
-const GalaxyClustersCount = document.getElementById('Galaxy Clusterscount');
-let GalaxyClustersCost = 24000;
-let GalaxyClustersOwned = 0;
-
-GalaxyClustersButton.addEventListener('click', () => {
-    if (cookie >= GalaxyClustersCost) {
-        cookie -= GalaxyClustersCost;
-        GalaxyClustersOwned++;
-        GalaxyClustersCount.textContent = GalaxyClustersOwned;
-        GalaxyClustersCost += 3000;
-        GalaxyClustersButton.textContent = `Galaxy Clusters: ${GalaxyClustersCost} stardust`;
-        showUpgradeEffect(GalaxyClustersButton);
+    clickForStardust() {
+        this.stardust += this.cookiePerClick;
+        this.renderStardust();
     }
-});
 
-let GalaxyClustersInterval = setInterval(() => {
-    cookie += 2000 * GalaxyClustersOwned;
-    cookieCount.textContent = cookie;
-}, 1000);
+    collectProduction() {
+        const production = this.upgrades
+            .filter(upgrade => upgrade instanceof ProductionUpgrade)
+            .reduce((total, upgrade) => total + upgrade.produce(), 0);
 
-const superclusterButton = document.getElementById('Superclusters');
-const superclusterCount = document.getElementById('Superclusterscount');
-let superclusterCost = 50000;
-let superclusterOwned = 0;
-
-superclusterButton.addEventListener('click', () => {
-    if (cookie >= superclusterCost) {
-        cookie -= superclusterCost;
-        superclusterOwned++;
-        superclusterCount.textContent = superclusterOwned;
-        superclusterCost += 5000;
-        superclusterButton.textContent = `Superclusters: ${superclusterCost} stardust`;
-        showUpgradeEffect(superclusterButton);
+        this.stardust += production;
+        this.renderStardust();
     }
-});
 
-let superclusterInterval = setInterval(() => {
-    cookie += 3000 * superclusterOwned;
-    cookieCount.textContent = cookie;
-}, 1000);
-
-const TheObservableUniverseButton = document.getElementById('The Observable Universe');
-const TheObservableUniverseCount = document.getElementById('The Observable Universecount');
-let TheObservableUniverseCost = 100000;
-let TheObservableUniverseOwned = 0;
-
-TheObservableUniverseButton.addEventListener('click', () => {
-    if (cookie >= TheObservableUniverseCost) {
-        cookie -= TheObservableUniverseCost;
-        TheObservableUniverseOwned++;
-        TheObservableUniverseCount.textContent = TheObservableUniverseOwned;
-        TheObservableUniverseCost += 10000;
-        TheObservableUniverseButton.textContent = `The Observable Universe: ${TheObservableUniverseCost} stardust`;
-        showUpgradeEffect(TheObservableUniverseButton);
+    renderStardust() {
+        this.cookieCount.textContent = this.stardust;
     }
-});
+}
 
-let TheObservableUniverseInterval = setInterval(() => {
-    cookie += 5000 * TheObservableUniverseOwned;
-    cookieCount.textContent = cookie;
-}, 1000);
-
-const multiverseButton = document.getElementById('Multiverse');
-const multiverseCount = document.getElementById('Multiversecount');
-let multiverseCost = 250000;
-let multiverseOwned = 0;
-
-multiverseButton.textContent = `Multiverse: ${multiverseCost} stardust`;
-
-multiverseButton.addEventListener('click', () => {
-    if (cookie >= multiverseCost) {
-        cookie -= multiverseCost;
-        multiverseOwned++;
-        multiverseCount.textContent = multiverseOwned;
-        multiverseCost += 25000;
-        multiverseButton.textContent = `Multiverse: ${multiverseCost} stardust`;
-        cookieCount.textContent = cookie;
-        showUpgradeEffect(multiverseButton);
-    }
-});
-
-let multiverseInterval = setInterval(() => {
-    cookie += 15000 * multiverseOwned;
-    cookieCount.textContent = cookie;
-}, 1000);
+new Game();
 
